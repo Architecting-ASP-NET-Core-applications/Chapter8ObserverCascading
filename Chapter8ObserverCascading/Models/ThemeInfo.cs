435a5001821bbbee253593bfa53c91bd99e0d1ad
BitawareUnleashed <dario.benevento@gmail.com>
2024-05-01 22:08:23 +0200

    Changed name of component

--- Chapter8ObserverCascading/Models/ThemeInfo.cs
+++ Chapter8ObserverCascading/Models/ThemeInfo.cs
@@ -5,13 +5,8 @@ namespace Chapter8ObserverCascading.Models;
 public class ThemeInfo
 {
     public string? MainTheme { get; set; }
-
-    public void ChangeToDarkTheme()
-    {
-        MainTheme = "div-container dark-theme";
-    }
-    public void ChangeToLiteTheme()
-    {
-        MainTheme = "div-container";
-    }
+    public void ChangeToDarkTheme() 
+        => MainTheme = "div-container dark-theme";
+    public void ChangeToLiteTheme() 
+        => MainTheme = "div-container";
 }
